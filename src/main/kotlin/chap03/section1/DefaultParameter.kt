e5d5a98405547c44bfd90426f36e989de183eff0
assakw@naver.com <>
2022-02-07 15:23:58 +0900

    오번
    오번 수정

--- src/main/kotlin/chap03/section1/DefaultParameter.kt
+++ src/main/kotlin/chap03/section1/DefaultParameter.kt
@@ -15,6 +15,9 @@ fun main() {
 fun add(name: String, email: String = "default"): Unit{
     val output = "${name}님의 이메일은 ${email}입니다."
     println(output)
+
+
+
 }
 
 fun defaultArgs(x: Int = 100, y: Int = 200){
