e27a4a96cf5a6b7db2032a70a3639087eb847f02
kkw <assakw@naver.com>
2022-03-23 23:30:11 +0900

    Squashed commit of the following:

--- src/main/kotlin/test/first/Test.kt
+++ src/main/kotlin/test/first/Test.kt
@@ -21,7 +21,7 @@ fun main() {
         it.age = 63
         // (T)->R 부분에서의 R에 해당하는 반환값 없음
     }
-    // 1,2,3,4,5
+    // 1,2
     println("$resultIt")
     // 1, 2, 3, 4
     println("$resultStr")
